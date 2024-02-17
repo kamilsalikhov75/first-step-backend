3b51c6f8544cd782182a2535de92d540b0c244ea
kasmil <kamilsalikhov75@gmail.com>
2024-02-17 21:10:39 +0300

    update cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: true });
+  app.enableCors({ origin: true, methods: ['GET', 'POST'], credentials: true });
   await app.listen(3000);
 }
 bootstrap();
